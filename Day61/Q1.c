d735ca35ec5f430ba45991faa27b49144749406d
Dev Saini <devsaini2512@gmail.com>
2026-04-02 12:00:00 +0530

    Day 61 DSA

---
+++ Day61/Q1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int adj[n][n];
+
+    // Initialize matrix with 0
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            adj[i][j] = 0;
+
+    int u, v;
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+
+        adj[u][v] = 1;
+        adj[v][u] = 1; // remove this line for directed graph
+    }
+
+    // Print matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
